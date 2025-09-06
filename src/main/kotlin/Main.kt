b0152763c68aa03e17b3b5f7c847021e9ffeebf7
faerberg <faerberg.pkgh@gmail.com>
2025-09-06 09:45:40 +0300

    imit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    print("Введите строку символов: ")
+    val text = readln()
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
