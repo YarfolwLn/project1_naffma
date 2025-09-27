6035f47e7f34cda5e7fe5d5cedce3e5976fe3541
faerberg <faerberg.pkgh@gmail.com>
2025-09-27 11:40:05 +0300

    all in

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,3 @@
-/*fun main() {
-    print("Введите строку: ")
-    val input = readln()
-
-    var result = ""
-    var count = 1
-
-    for (i in 1 until input.length) {
-        if (input[i] == input[i - 1]) {
-            count++
-        } else {
-            result += input[i - 1]
-            if (count > 1) {
-                result += count.toString()
-            }
-            count = 1
-        }
-    }
-
-    result += input[input.length - 1]
-    if (count > 1) {
-        result += count.toString()
-    }
-
-    println("Результат: $result")
-}*/
-
 fun main() {
     println("=== Меню программ ===\n")
 
